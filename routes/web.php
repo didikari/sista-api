1f5f01774e01b8b2c42597b906940a0a76dd0404
Didik Ariyanto <didikariyanto301@gmail.com>
2025-02-24 09:42:31 +0700

    remove route web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::fallback(function () {
+//     abort(404, 'Not Found');
+// });
